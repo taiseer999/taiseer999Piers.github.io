5dcf3770a0e565d661025838561669b06e2291a0
Abukarim <118752825+taiseer999@users.noreply.github.com>
2026-07-11 15:17:09 +0300

    fx

--- repo/script.skinshortcuts.22/resources/lib/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Skin Shortcuts library."""
--- repo/script.skinshortcuts.22/resources/lib/skinshortcuts/builders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Output builders."""
-
-from __future__ import annotations
-
-from .includes import IncludesBuilder
-from .template import TemplateBuilder
-from .views import ViewExpressionBuilder
-
-__all__ = ["IncludesBuilder", "TemplateBuilder", "ViewExpressionBuilder"]
